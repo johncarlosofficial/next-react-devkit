dbb0b528cb718274b98f71d6edc781a13beedddf
John Carlos <79121349+johncarlosofficial@users.noreply.github.com>
2025-01-15 08:11:05 -0300

    build(app): configure app router

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "John Carlos",
+  description: "Brazilian | Software Engineer | Digital Content Producer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.variable}>{children}</body>
+    </html>
+  );
+}
